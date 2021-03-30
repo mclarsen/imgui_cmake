275d9fb266aeffdde676927d1e5b6ca512ad7ab8
Matt Larsen <larsen30@llnl.gov>
2021-03-30 07:53:44 -0700

    add examples

--- src/examples/implot_demo.cpp
+++ src/examples/implot_demo.cpp
@@ -3,11 +3,11 @@
 
 
 #include <imgui.h>
+#include <implot.h>
 #include <GLFW/glfw3.h>
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl2.h>
 
-//#include "imgui/imgui_impl_metal.h"
 static void glfw_error_callback(int error, const char* description)
 {
   fprintf(stderr, "Glfw Error %d: %s\n", error, description);
@@ -24,7 +24,7 @@ int main(int, char**)
     return 1;
   }
 
-  GLFWwindow* window = glfwCreateWindow(1280, 720, "Wolf Vision", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "IMPlot Demo", NULL, NULL);
   if (window == NULL)
   {
     return 1;
@@ -33,10 +33,10 @@ int main(int, char**)
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1); // Enable vsync
 
-  std::cout<<"Hello\n";
   // Setup ImGui context
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
+  ImPlot::CreateContext();
   ImGuiIO& io = ImGui::GetIO(); (void)io;
 
   ImGui::StyleColorsDark();
@@ -46,7 +46,6 @@ int main(int, char**)
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   ImGui_ImplOpenGL2_Init();
 
-  //MainWindow main_window;
   while (!glfwWindowShouldClose(window))
   {
     glfwPollEvents();
@@ -56,10 +55,8 @@ int main(int, char**)
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    bool show_demo = true;
-    ImGui::ShowDemoWindow(&show_demo);
-
-    //main_window.render();
+    bool implot_demo = true;
+    ImPlot::ShowDemoWindow(&implot_demo);
 
     // Rendering
     ImGui::Render();
@@ -68,19 +65,17 @@ int main(int, char**)
     glViewport(0, 0, display_w, display_h);
     glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT);
-    //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL2+ code.
     ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 
     glfwMakeContextCurrent(window);
     glfwSwapBuffers(window);
   }
 
-  //main_window.kill();
-
   // Cleanup
   ImGui_ImplOpenGL2_Shutdown();
   ImGui_ImplGlfw_Shutdown();
   ImGui::DestroyContext();
+  ImPlot::DestroyContext();
 
   glfwDestroyWindow(window);
   glfwTerminate();
