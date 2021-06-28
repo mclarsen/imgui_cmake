c6eebce79e13cf8db4f0f88d37c36df4aaac8693
mclarsen <larsen30@llnl.gov>
2021-06-28 08:08:45 -0700

    adding pascal config

---
+++ host_configs/pascal.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/gcc" CACHE PATH "")
+set(CMAKE_CXX_COMPILER "/usr/tce/packages/gcc/gcc-8.3.1/bin/g++" CACHE PATH "")
+# glfw
+set(GLFW3_DIR "/usr/workspace/showcase/glfw/install" CACHE PATH "")
